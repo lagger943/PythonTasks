102101fb20d1b266c58f2b83f6b92124fb2f5117
Martin Mitov <lagger943@gmail.com>
2021-06-14 17:48:08 +0300

    Reafactor of the tasks

--- Part 1/Task4.py
+++ Part 1/Task4.py
@@ -1,3 +1,3 @@
 string = "S$HkrH ellolwor lwdomrtlede!ad"
-print(string[2]+string[7:11]+string[15]+string[12:15]+string[23:28:2])
+print(string[5:29:2])
 
--- Part 1/Task7.py
+++ Part 1/Task7.py
@@ -1,36 +1,4 @@
 list_one = ['six', 'three', 'two', 'one']
 list_two = ['four', 'seven', 'five']
-place_holder_one = None
-place_holder_two = None
-output = []
-output.insert(0,list_one[3])
-output.insert(1,list_one[2])
-output.insert(2,list_one[1])
-output.insert(3,list_two[0])
-output.insert(4,list_two[2])
-output.insert(5,list_one[0])
-output.insert(6,list_two[1])
-print(output)
-
-
-/----------------------------/
-
-
-list_one = ['six', 'three', 'two', 'one']
-list_two = ['four', 'seven', 'five']
-output = []
-ph_one = list_one[3]
-ph_two = list_one[2]
-ph_three = list_one[1]
-ph_four = list_two[0]
-ph_five = list_two[2]
-ph_six = list_one[0]
-ph_seven = list_two[1]
-output.append(ph_one)
-output.append(ph_two)
-output.append(ph_three)
-output.append(ph_four)
-output.append(ph_five)
-output.append(ph_six)
-output.append(ph_seven)
-print(output)
+final_list = list_two + list_one
+print(final_list[-1:-4:-1] + final_list[0:3:2] + final_list[-4:-7:-2])
