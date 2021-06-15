a2038641e43eb1aca7e615fee6b09c5c7ea62ad9
Martin Mitov <lagger943@gmail.com>
2021-06-15 11:00:16 +0300

    Refactor

--- Part 1/Task2.py
+++ Part 1/Task2.py
@@ -4,5 +4,5 @@ shipping_cost = 5.5 #EUROS
 price_bgn = price*1.67 #Converting to bgn
 shipping_bgn = shipping_cost*1.96 #Converting to bgn
 total_price=price_bgn+shipping_bgn #Total sum with shipping
-print(" Price without shippping",price_bgn,"Leva\n","Price with shipping",total_price,"Leva")
+print(f" Price without shippping {price_bgn} Leva\n Price with shipping {total_price} Leva")
 
